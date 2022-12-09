633e9c104a1cd09a0e26923a163fb4df120dec14
Nacho <isanccal@myuax.com>
2022-12-09 10:00:27 +0100

    Cambio final

--- fibonacci.py
+++ fibonacci.py
@@ -1,12 +1,7 @@
-def fibonacci(numero):
-    if numero == 0:
-        return 0
-    elif numero == 1:
-        return 1
-    else:
-        return fibonacci(numero-1) + fibonacci(numero-2)
-for numero in range(1000000):
-    fibonacci(1000000)
-
-
-    
+def fib(n):                # escribe la serie de Fibonacci hasta n
+    a, b = 0,1             # a y b son los dos primeros numeros de la serie
+    while a < n:           # mientras a sea menor que n
+        print(a, end=' ')      # imprime a
+        a, b = b, a+b          # a toma el valor de b y b toma el valor de a+b
+    print()                    # imprime un salto de linea
+fib(1000000)                   # llama a la funcion fib con el argumento 1000000
